b90318369e23871600ba38b516da9d340daa8480
Rahul Krishna <rkbalu524@gmail.com>
2018-12-25 22:11:29 +0530

    ✨ Complete Poem Service with request params

--- frontend/src/app/poem.service.ts
+++ frontend/src/app/poem.service.ts
@@ -1,9 +1,28 @@
 import "rxjs/add/operator/toPromise";
 import { Injectable } from "@angular/core";
+import { Http, Headers } from "@angular/http";
+
+import { Poem } from "./poem";
 
 @Injectable({
   providedIn: "root"
 })
 export class PoemService {
-  constructor() {}
+  private headers = new Headers({ "Content-Type": "application/json" });
+  private url = "api/v1/card";
+  private handleError(error: any): Promise<any> {
+    console.error(error);
+    return Promise.reject(error.message || error);
+  }
+  constructor(private http: Http) {}
+
+  generate(poem: Poem): Promise<any> {
+    return this.http
+      .post(this.url, JSON.stringify(poem), {
+        headers: this.headers
+      })
+      .toPromise()
+      .then(resp => resp.json())
+      .catch(this.handleError);
+  }
 }
