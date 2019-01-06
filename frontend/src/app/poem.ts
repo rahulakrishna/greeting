6545e9f56d7440e47bf2e2d719f7c1454f94201c
Rahul Krishna <rkbalu524@gmail.com>
2019-01-06 17:30:23 +0530

    🚛 Replacing G. Sankarakkurupp with William Wordsworth
    🚛 Fixes the possible font conflict that may arise

--- frontend/src/app/poem.ts
+++ frontend/src/app/poem.ts
@@ -4,21 +4,17 @@ export class Poem {
   author: string;
 
   constructor(title?: string, body?: string[], author?: string) {
-    this.title = title || "സൂര്യകാന്തി";
+    this.title = title || "Solitary Reaper";
     this.body = body || [
-      "മന്ദമന്ദമെന്‍ താഴും മുഗ്ദമാം മുഖം പൊക്കി-",
-      "സ്സുന്ദരദിവാകരന്‍ ചോദിച്ചൂ മധുരമായ്‌:",
-      "“ആരു നീയനുജത്തീ? നിര്‍ന്നിമേഷയായെന്തെന്‍",
-      "തേരുപോകവെ നേരെ നോക്കിനില്‍ക്കുന്നൂ ദൂരേ?",
-      "സൗമ്യമായ്‌ പിന്നെപ്പിന്നെ വിടരും കണ്ണാല്‍ സ്നേഹ-",
-      "രമ്യമായ്‌ വീക്ഷിയ്ക്കുന്നൂ തിരിഞ്ഞു തിരിഞ്ഞെന്നെ;",
-      "വല്ലതും പറയുവാനാഗ്രഹിയ്ക്കുന്നുണ്ടാവാ-",
-      "മില്ലയോ? തെറ്റാണൂഹമെങ്കിൽ, ഞാന്‍ ചോദിച്ചീല.”",
-      "ഒന്നുമുത്തരം തോന്നീലെങ്ങനെ തോന്നും? സര്‍വ്വ-",
-      "സന്നുതന്‍ സവിതാവെങ്ങു നിര്‍ഗന്ധം പുഷ്പം!",
-      "അര്യമാവിനെ സ്നേഹിക്കുന്ന ധിക്കാരത്തിന്നു",
-      "സൂര്യകാന്തിയെന്നെന്നെ പ്പുച്ഛിച്ചതാണീ ലോകം!"
+      "Behold her, single in the field, ",
+      "Yon solitary Highland Lass! ",
+      "Reaping and singing by herself; ",
+      "Stop here, or gently pass! ",
+      "Alone she cuts and binds the grain, ",
+      "And sings a melancholy strain; ",
+      "O listen! for the Vale profound ",
+      "Is overflowing with the sound. "
     ];
-    this.author = author || "ജി. ശങ്കരക്കുറുപ്പ്";
+    this.author = author || "William Wordsworth";
   }
 }
