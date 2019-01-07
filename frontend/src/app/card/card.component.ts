a0a776dc78422abf794ca9c7e58464b24b28cfe0
Rahul Krishna <rkbalu524@gmail.com>
2019-01-07 12:05:57 +0530

    🎨 Showing the final image in the cards

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-card",
@@ -6,7 +7,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./card.component.scss"]
 })
 export class CardComponent implements OnInit {
-  constructor() {}
+  imagePath = "";
 
-  ngOnInit() {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    let fieldId = this.route.snapshot.paramMap.get("path");
+    this.imagePath = `download/${fieldId}.png`;
+    console.log("ngOnInit", this.imagePath);
+  }
 }
