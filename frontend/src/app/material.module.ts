29429e7ff36ec983d962d778db87d824f01d5add
Rahul Krishna <rkbalu524@gmail.com>
2019-01-14 09:16:29 +0530

    ✨ Dark Mode toggle switch
    ✨ Adding Hammer.js for the touch actions on Switch
    🎨 Style changes for Dark Mode and Light Mode
    ✨ Logic for Dark Mode inside app.component.html and home.component.html

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  imports: [MatToolbarModule, MatInputModule, MatButtonModule],
-  exports: [MatToolbarModule, MatInputModule, MatButtonModule]
+  imports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSlideToggleModule
+  ]
 })
 export class MaterialModule {}
